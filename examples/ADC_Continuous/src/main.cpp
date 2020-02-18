41db0297f72164a1652e7cfda4dcef4376f41b5c
unknown <gitarmpit@gmail.com>
2020-02-18 00:10:25 -0500

    ADC

--- examples/ADC_Continuous/src/main.cpp
+++ examples/ADC_Continuous/src/main.cpp
@@ -1,27 +1,3 @@
-/*
- * The MIT License (MIT)
- * 
- * Copyright (c) 2015 igorS
- *  
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
 #include "adc_cpp.h"
 #include "gpio_cpp.h"
 #include "RCC_Helper.h"
@@ -61,8 +37,8 @@ public:
 void Test_Continuous_Conversion()
 {
 #if defined(STM32F1)
-    //RCC_EnableHSI_72Mhz_AHB_72Mhz_APB1_36MHz_APB2_72MHz();
-    RCC_EnableHSI_24Mhz();
+    RCC_EnableHSI_64Mhz_AHB_32Mhz_APB1_16MHz_APB2_16MHz();
+    // RCC_EnableHSI_24Mhz();
 #elif defined(STM32F4)
     RCC_EnableHSI_168Mhz();
     RCC_SetAPB2PrescalerDiv16();
