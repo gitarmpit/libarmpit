7d04e4d46738e93768bc757dcda1151bbe31e317
gitarmpit@gmail.com <gitarmpit@gmail.com>
2023-02-25 20:08:13 -0500

    update

--- examples/DAC/src/main.cpp
+++ examples/DAC/src/main.cpp
@@ -32,7 +32,7 @@ static void GenerateData()
 {
     for (uint32_t i = 0; i <= nsteps; i += 1)
     {
-        data[i] = (int) ((sin(2 * 3.14159 * (float) i / (float) nsteps) + 1.)
+        data[i] = (int) ((sin(2 * M_PI * (float) i / (float) nsteps) + 1.)
                 * (float) (0xfff + 1) / 2.);
     }
 }
@@ -165,10 +165,88 @@ static void DAC_Test_manual()
 
 }
 
+static void DAC_Test_DMA2()
+{
+
+
+    GenerateData();
+
+    GPIOA* portA = GPIOA::GetInstance();
+    portA->EnablePeripheralClock(true);
+    portA->SetupGPIO_InAnalog(GPIO_PIN4); //DAC pin
+
+    DAC* dac = DAC::GetInstance();
+    dac->EnablePeripheralClock(true);
+    dac->SetIrqHandler(new DAC_IRQ_Handler1());
+
+#if 0
+
+    DAC_Channel* dacCh = dac->GetChannel(DAC_CHANNEL_1);
+    dacCh->SelectTriggerTimer6();
+    //dacCh->EnableUnderrunInterrupt();
+    dacCh->SelectDataRegister12R();
+
+    uint32_t dacDataAddr = (uint32_t) dacCh->GetDataRegisterAddress();
+
+    DMA1* dma1 = DMA1::GetInstance();
+    dma1->EnablePeripheralClock(true);
+
+#ifdef STM32F1
+    DMA_Channel* dmaCh3 = dma1->GetChannel(DMA_CHANNEL_3);
+    dmaCh3->SetPeripheralAddress(dacDataAddr);
+    dmaCh3->SetMemoryAddress((uint32_t) data);
+    dmaCh3->SetCircularMode();
+    dmaCh3->SetDirection_MemoryToPeripheral();
+    dmaCh3->SetMemorySize_16bits();
+    dmaCh3->SetPeripheralSize_16bits();
+    dmaCh3->SetNumberOfData(nsteps);
+    dmaCh3->SetMemoryIncrementMode();
+    dmaCh3->SetPriorityHigh();
+#elif defined(STM32F4)
+    DMA_Stream* s = dma1->GetStream(DMA_STREAM_5);
+    s->SetChannel (DMA_CHANNEL_7);
+    s->SetPeripheralAddress(dacDataAddr);
+    s->SetMemory0Address((uint32_t) data);
+    s->SetCircularMode(TRUE);
+    s->SetDirection_MemoryToPeripheral();
+    s->SetMemorySize_16bits();
+    s->SetPeripheralSize_16bits();
+    s->SetNumberOfData(nsteps);
+    s->SetMemoryIncrementMode(TRUE);
+    s->SetPriorityVeryHigh();
+    s->EnableStream(TRUE);
+#endif
+
+
+    dacCh->EnableChannel();
+    dacCh->EnableDMA();
+
+    //TIM6* tim = TIM6::GetInstance();
+    //tim->EnablePeripheralClock(TRUE);
+
+    uint32_t sin_period_us = 8;
+
+    uint32_t update_period_ns = sin_period_us * 1000 / nsteps;
+
+    //tim6->SetUpdatePeriod_us(1); //84=100us, 21=25us, 8~11us
+    //tim6->SetAutoReloadValue(6);
+    //tim->SetUpdatePeriod_ns (update_period_ns);
+
+    //tim->EnableAutoPreload(TRUE);
+    //tim->SetMMS_Update();
+    //tim->EnableCounter(TRUE);
+
+    while(1)
+        ;
+#endif
+
+}
+
+
 int main()
 {
 
     RCC_EnableHSI_64Mhz_AHB_64Mhz_APB1_32MHz_APB2_64MHz();
 
-    DAC_Test_DMA();
+    DAC_Test_DMA2();
 }
--- lib/__gnu_stubs.c
+++ lib/__gnu_stubs.c
@@ -40,6 +40,33 @@ __attribute__ ((used))
 //#endif
 
 
+// stubs 
+
+int _close(int file) {
+  return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+  st->st_mode = S_IFCHR;
+  return 0;
+}
+
+int _isatty(int file) {
+  return 1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+  return 0;
+}
+
+int _read(int file, char *ptr, int len) {
+  return 0;
+}
+
+int _write(int file, char *ptr, int len) {
+  return 0;
+}
+
 
 
 #endif
