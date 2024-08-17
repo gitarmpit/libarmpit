3804d90b777b67e3c1ccc4329263309b180d0350
gitarmpit <gitarmpit@gmail.com>
2024-08-17 12:22:28 -0400

    add

---
+++ _projects/lib/stm32f7xx/lib/system_stm32f7xx.c
@@ -0,0 +1,100 @@
+#include "stm32f7xx.h"
+
+#if !defined  (HSE_VALUE) 
+//  #define HSE_VALUE    ((uint32_t)25000000) /*!< Default value of the External oscillator in Hz */
+#error HSE_VALUE not defined
+#endif /* HSE_VALUE */
+
+#if !defined  (HSI_VALUE)
+  #define HSI_VALUE    ((uint32_t)16000000) /*!< Value of the Internal oscillator in Hz*/
+#endif /* HSI_VALUE */
+
+
+/* Note: Following vector table addresses must be defined in line with linker
+         configuration. */
+/*!< Uncomment the following line if you need to relocate the vector table
+     anywhere in Flash or Sram, else the vector table is kept at the automatic
+     remap of boot address selected */
+/* #define USER_VECT_TAB_ADDRESS */
+
+#if defined(USER_VECT_TAB_ADDRESS)
+/*!< Uncomment the following line if you need to relocate your vector Table
+     in Sram else user remap will be done in Flash. */
+/* #define VECT_TAB_SRAM */
+#if defined(VECT_TAB_SRAM)
+#define VECT_TAB_BASE_ADDRESS   RAMDTCM_BASE    /*!< Vector Table base address field.
+                                                     This value must be a multiple of 0x200. */
+#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
+                                                     This value must be a multiple of 0x200. */
+#else
+#define VECT_TAB_BASE_ADDRESS   FLASH_BASE      /*!< Vector Table base address field.
+                                                     This value must be a multiple of 0x200. */
+#define VECT_TAB_OFFSET         0x00000000U     /*!< Vector Table base offset field.
+                                                     This value must be a multiple of 0x200. */
+#endif /* VECT_TAB_SRAM */
+#endif /* USER_VECT_TAB_ADDRESS */
+
+uint32_t SystemCoreClock = 16000000;
+const uint8_t AHBPrescTable[16] = {0, 0, 0, 0, 0, 0, 0, 0, 1, 2, 3, 4, 6, 7, 8, 9};
+const uint8_t APBPrescTable[8] = {0, 0, 0, 0, 1, 2, 3, 4};
+
+void SystemInit(void)
+{
+  /* FPU settings ------------------------------------------------------------*/
+#if (__FPU_PRESENT == 1) && (__FPU_USED == 1)
+  SCB->CPACR |= ((3UL << 10*2)|(3UL << 11*2));  /* set CP10 and CP11 Full Access */
+#endif
+
+  /* Configure the Vector Table location -------------------------------------*/
+#if defined(USER_VECT_TAB_ADDRESS)
+  SCB->VTOR = VECT_TAB_BASE_ADDRESS | VECT_TAB_OFFSET; /* Vector Table Relocation in Internal SRAM */
+#endif /* USER_VECT_TAB_ADDRESS */
+}
+
+void SystemCoreClockUpdate(void)
+{
+  uint32_t tmp = 0, pllvco = 0, pllp = 2, pllsource = 0, pllm = 2;
+  
+  /* Get SYSCLK source -------------------------------------------------------*/
+  tmp = RCC->CFGR & RCC_CFGR_SWS;
+
+  switch (tmp)
+  {
+    case 0x00:  /* HSI used as system clock source */
+      SystemCoreClock = HSI_VALUE;
+      break;
+    case 0x04:  /* HSE used as system clock source */
+      SystemCoreClock = HSE_VALUE;
+      break;
+    case 0x08:  /* PLL used as system clock source */
+
+      /* PLL_VCO = (HSE_VALUE or HSI_VALUE / PLL_M) * PLL_N
+         SYSCLK = PLL_VCO / PLL_P
+         */    
+      pllsource = (RCC->PLLCFGR & RCC_PLLCFGR_PLLSRC) >> 22;
+      pllm = RCC->PLLCFGR & RCC_PLLCFGR_PLLM;
+      
+      if (pllsource != 0)
+      {
+        /* HSE used as PLL clock source */
+        pllvco = (HSE_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);
+      }
+      else
+      {
+        /* HSI used as PLL clock source */
+        pllvco = (HSI_VALUE / pllm) * ((RCC->PLLCFGR & RCC_PLLCFGR_PLLN) >> 6);      
+      }
+
+      pllp = (((RCC->PLLCFGR & RCC_PLLCFGR_PLLP) >>16) + 1 ) *2;
+      SystemCoreClock = pllvco/pllp;
+      break;
+    default:
+      SystemCoreClock = HSI_VALUE;
+      break;
+  }
+  /* Compute HCLK frequency --------------------------------------------------*/
+  /* Get HCLK prescaler */
+  tmp = AHBPrescTable[((RCC->CFGR & RCC_CFGR_HPRE) >> 4)];
+  /* HCLK frequency */
+  SystemCoreClock >>= tmp;
+}
---
+++ _projects/lib/stm32f7xx/libc/stm32f7xx_it.c
@@ -0,0 +1,165 @@
+void NMI_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void HardFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void MemManage_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void BusFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void UsageFault_Handler(void)
+{
+  while (1)
+  {
+  }
+}
+
+void SVC_Handler(void)
+{
+}
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
+
+void SysTick_Handler(void)
+{
+}
+
+void ADC_IRQHandler(void)
+{
+}
+
+void TIM1_BRK_TIM9_IRQHandler(void)
+{
+}
+
+void TIM1_UP_TIM10_IRQHandler(void)
+{
+}
+
+void TIM1_TRG_COM_TIM11_IRQHandler(void)
+{
+}
+
+void TIM2_IRQHandler(void)
+{
+}
+
+void TIM4_IRQHandler(void)
+{
+}
+
+void I2C1_EV_IRQHandler(void)
+{
+}
+
+void I2C1_ER_IRQHandler(void)
+{
+}
+
+void I2C2_EV_IRQHandler(void)
+{
+}
+
+void I2C2_ER_IRQHandler(void)
+{
+}
+
+void SPI1_IRQHandler(void)
+{
+}
+
+void SPI2_IRQHandler(void)
+{
+}
+
+void USART1_IRQHandler(void)
+{
+}
+
+void USART2_IRQHandler(void)
+{
+}
+
+void USART3_IRQHandler(void)
+{
+}
+
+void TIM8_BRK_TIM12_IRQHandler(void)
+{
+}
+
+void TIM8_UP_TIM13_IRQHandler(void)
+}
+
+void TIM8_TRG_COM_TIM14_IRQHandler(void)
+{
+}
+
+void TIM8_CC_IRQHandler(void)
+{
+}
+
+void TIM5_IRQHandler(void)
+{
+}
+
+void SPI3_IRQHandler(void)
+{
+}
+
+void UART5_IRQHandler(void)
+{
+}
+
+void TIM6_DAC_IRQHandler(void)
+{
+}
+
+void TIM7_IRQHandler(void)
+{
+}
+
+void USART6_IRQHandler(void)
+{
+}
+
+void I2C3_EV_IRQHandler(void)
+{
+}
+
+void I2C3_ER_IRQHandler(void)
+{
+}
+
+void UART8_IRQHandler(void)
+{
+}
+
+void SPI4_IRQHandler(void)
+{
+}
